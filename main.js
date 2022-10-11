f3e9512e15528418bc64f9a41570ed601f81666b
Diogo <okekechidiogoc@gmail.com>
2022-10-11 15:46:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+ //Getting required elements
+
+let hamburgerMenu = document.querySelector('#hamburger-menu');
+let navContent = document.querySelector('.nav-content');
+let closeMenu = document.querySelector('#close-menu');
+let featurePTag = document.querySelector('.feature-pTag');
+let companyPTag = document.querySelector('.company-pTag');
+let featuresSubContent = document.querySelector('.features-subContent');
+let companySubContent = document.querySelector('.company-subContent');
+let featureCaret = document.querySelector('.feature-caret');
+let companyCaret = document.querySelector('.company-caret');
+
+
+// HamburgerMenu Onclick
+
+hamburgerMenu.onclick = () => {
+ navContent.style.display = 'block';
+ navContent.style.pointerEvents = 'auto';
+}
+
+
+// CloseMenu Onclick
+
+closeMenu.onclick = () => {
+ navContent.style.display = 'none';
+ navContent.style.pointerEvents = 'none';
+ featuresSubContent.style.display = 'none';
+ featuresSubContent.style.pointerEvents = 'none';
+ companySubContent.style.display = 'none';
+ companySubContent.style.pointerEvents = 'none';
+ featureCaret.style.transform = 'rotate(180deg)';
+ companyCaret.style.transform = 'rotate(180deg)';
+}
+
+
+//FeatureSection Onclick
+
+featurePTag.onclick = () => {
+ 
+ if(getComputedStyle(featuresSubContent).display === 'block'){
+  featuresSubContent.style.display = 'none';
+  featuresSubContent.style.pointerEvents = 'auto';
+  featureCaret.style.transform = 'rotate(180deg)';
+ }
+ else{
+  featuresSubContent.style.display = 'block';
+  featuresSubContent.style.pointerEvents = 'auto';
+  featureCaret.style.transform = 'rotate(360deg)';
+ }
+
+}
+
+
+//CompanySection Onclick
+
+companyPTag.onclick = () => {
+ 
+ if(getComputedStyle(companySubContent).display === 'block'){
+  companySubContent.style.display = 'none';
+  companySubContent.style.pointerEvents = 'auto';
+  companyCaret.style.transform = 'rotate(180deg)';
+ }
+ else{
+  companySubContent.style.display = 'block';
+  companySubContent.style.pointerEvents = 'auto';
+  companyCaret.style.transform = 'rotate(360deg)';
+ }
+
+}
+
+
